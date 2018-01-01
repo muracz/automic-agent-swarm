ca631a33a756e81c6e88b6bda63f9c5480e77269
Hrv <harv@harv.pl>
2018-01-01 15:10:35 +0100

    Initial commit

---
+++ agents/unix_alpine/bins/start.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+# Global settings normally to be set with environment variables
+
+# prefix="DA"
+# system="AUTOMIC1"
+# cp="localhost:2017"
+
+
+# Set all the necessary variables
+path="/Automic"
+exec="ucxjlx6"
+sufix=$(hostname | cut -c1-8)
+agent="${prefix}_${sufix}"
+
+# Print out the global variables
+echo "Starting agent with the following setup:"
+echo " System:  ${system}"
+echo " CP:      ${cp}"
+echo " Prefix:  ${prefix}"
+echo "===================="
+
+# Generate the configuration file
+cd ${path}
+template=$(find . -name "*_template")
+config=$(echo ${template} | sed 's/_template//g')
+
+cat ${template} | sed -e "s/#AGENT#/${agent}/g" -e "s/#CP#/${cp}/g" -e "s/#SYSTEM#/${system}/g" > ${config}
+
+# Set the necesary permissions
+chown root ${path}/bin/${exec}
+chmod 4755 ${path}/bin/${exec}
+ls -l ${path}/bin/${exec}
+
+# Start the agent
+${path}/bin/${exec}
